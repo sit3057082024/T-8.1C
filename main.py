a75dc6e62084792ba10bbf011b04dd89cc6b0840
namanyash <naman_yash@yahoo.in>
2024-04-30 12:47:58 +1000

    Llama3 update

--- main.py
+++ main.py
@@ -6,21 +6,17 @@ import os
 from gradientai import Gradient
 
 app = Flask(__name__)
-
-
-new_model_adapter = None
-
+base_model = None
 token = 'LXkZBp5a1nGCP16xM7QieYKNz2Ns0tOW'
 workspace_id = 'b5f958d9-ffd4-41bb-a492-704114e02c8e_workspace'
 
 os.environ['GRADIENT_ACCESS_TOKEN'] = token
 os.environ['GRADIENT_WORKSPACE_ID'] = workspace_id
 
-def prepareLlamaBot(name):
+def prepareLlamaBot():
+    global base_model
     gradient = Gradient()
-    base_model = gradient.get_base_model(base_model_slug="llama2-7b-chat")
-    global new_model_adapter
-    new_model_adapter = base_model.create_model_adapter(name=name)
+    base_model = gradient.get_base_model(base_model_slug="llama3-8b-chat")
 
 @app.route('/')
 def index():
@@ -28,37 +24,35 @@ def index():
 
 @app.route('/chat', methods=['POST'])
 def chat():
-    global new_model_adapter, user_message, chat_history
+    global base_model, user_message, chat_history
     data = request.get_json()
+
     if 'userMessage' not in data or not isinstance(data['userMessage'], str):
         return jsonify({'error': 'userMessage must be a string'}), 400
+
     if 'chatHistory' not in data or not isinstance(data['chatHistory'], list):
         return jsonify({'error': 'chatHistory must be a list'}), 400
+
     if not all(isinstance(item, dict) and 'User' in item and 'Llama' in item for item in data['chatHistory']):
         return jsonify({'error': 'chatHistory must be a list of dictionaries with keys User and Llama'}), 400
 
     user_message = data['userMessage']
     chat_history = data['chatHistory']
-
     chat_history_str = '\n'.join([f"{item['User']} - {item['Llama']}" for item in chat_history])
 
     QUERY = f"[INST]GIVEN THE CHAT HISTORY:\n{chat_history_str}\nAND THE LATEST MESSAGE FROM USER:\n{user_message}\nGIVE A RESPONSE TO THE USER\n[/INST]"
 
-    response = new_model_adapter.complete(query=QUERY, max_generated_token_count=500).generated_output
+    response = base_model.complete(query=QUERY, max_generated_token_count=500).generated_output
 
     return jsonify({'message': response}), 200
 
-
-
 if __name__ == '__main__':
-    default_name = f"Llama_{datetime.now().strftime('%Y%m%d_%H%M%S')}_{socket.gethostname()}"
-    
     parser = argparse.ArgumentParser()
-    parser.add_argument('--name', default=default_name, help='Specify a name')
+    parser.add_argument('--port', type=int, default=5000, help='Specify the port number')
     args = parser.parse_args()
 
-    port_num = 5000
-    print(f"Starting Llama bot with name {args.name}...\n This may take a while.")
-    prepareLlamaBot(args.name)
+    port_num = args.port
+    print("Starting Llama bot...\n This may take a while.")
+    prepareLlamaBot()
     print(f"App running on port {port_num}")
     app.run(port=port_num)
